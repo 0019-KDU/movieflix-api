bf05e60b51a6c94404fd0915a68db90319dddde3
CR Hapuarachchi <39-bit-0019@kdu.ac.lk>
2024-09-19 19:38:36 +0530

    Initial commit

---
+++ src/main/java/com/moiveflix/MoiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.moiveflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoiveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moiveflix/MoiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moiveflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoiveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
